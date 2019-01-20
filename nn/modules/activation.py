1d4fbc0ce6e7e64595e450bdc6b847a02aa2c16a
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-01-20 00:55:19 -0600

    Add files via upload

---
+++ nn/modules/activation.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*- 
+from .module import Module 
+from ..functions import sin, tanh, relu, leakyrelu, elu, sigmoid 
+ 
+class Sin(Module): 
+    '''Trigonometric sine, element-wise.\n 
+    ''' 
+    def __init__(self): 
+        super().__init__()     
+         
+    def forward(self, x): 
+        return sin(x) 
+ 
+class Tanh(Module): 
+    '''Compute hyperbolic tangent element-wise.\n 
+    ''' 
+    def __init__(self): 
+        super().__init__()     
+         
+    def forward(self, x): 
+        return tanh(x) 
+ 
+class ReLU(Module): 
+    '''Rectified Linear Unit\n 
+    ''' 
+    def __init__(self): 
+        super().__init__()     
+         
+    def forward(self, x): 
+        return relu(x) 
+ 
+class LeakyReLU(Module): 
+    '''Leaky Rectified Linear Unit     
+    ''' 
+    def __init__(self): 
+        super().__init__() 
+ 
+    def forward(self, x): 
+        return leakyrelu(x) 
+ 
+class ELU(Module): 
+    '''Exponential Linear Unit\n 
+ 
+    Args: 
+        a (float): a positive parameter that scales the negative region of the input so that the output region will be [-a, +inf] 
+    ''' 
+    def __init__(self, a): 
+        super().__init__() 
+        self.a = a     
+         
+    def forward(self, x): 
+        return elu(x, self.a) 
+ 
+class Sigmoid(Module): 
+    '''A logistic function\n 
+    ''' 
+    def __init__(self): 
+        super().__init__()     
+         
+    def forward(self, x): 
+        return sigmoid(x) 
