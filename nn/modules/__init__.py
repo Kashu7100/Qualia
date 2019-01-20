7ca5d4ea5386d45361d24cf41dbe377546c5426b
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-01-20 10:52:06 -0600

    Delete __init__.py

--- nn/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*- 
-from .module import * 
-from .linear import * 
-from .conv import * 
-from .pool import * 
-from .normalization import * 
-from .loss import * 
-from .activation import *
