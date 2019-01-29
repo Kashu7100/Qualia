0c82ce49da3d261b8a037a57a7067ad946f96bb5
Kashu <39104615+Kashu7100@users.noreply.github.com>
2019-01-28 22:44:25 -0600

    Create rnn.py

---
+++ qualia/nn/modules/rnn.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*- 
+import numpy as np 
+import math 
+from .module import Module 
+from ..functions import tensordot, tanh, concat, reshape, repeat
+from ...autograd import Variable, Backward
